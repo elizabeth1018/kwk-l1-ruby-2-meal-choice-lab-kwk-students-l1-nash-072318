508389f4390a2e9a1d4b4edbb0fde82ccdd814e2
elizabeth1018 <lizz_101@icloud.com>
2018-07-24 15:33:52 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -23,7 +23,8 @@ puts snacks("Swedish fish")
 ## This returns "Any time, is the right time for Sweedish fish!"
 
 # call your methods here
-puts breakfast
+puts breakfast(toast)
+puts lunch()
 
 
 # Call your methods without any arguments here
